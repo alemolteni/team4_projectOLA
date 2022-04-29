2cb1351998f02b79b50642f8763b63e08106eade
GabrieleCurti <gabriele.curti@mail.polimi.it>
2022-04-29 12:38:40 +0200

    Added Test for GreedyLearner Added simulation for GreedyLearner BugFixed

--- Learner/GreedyLearner.py
+++ Learner/GreedyLearner.py
@@ -1,5 +1,7 @@
 import numpy as np
-from Environment import *
+
+
+
 
 class GreedyLearner:
 
@@ -8,7 +10,6 @@ class GreedyLearner:
         self.t = -1 #per provare la configurazione tutti 0
         self.optimal_configuration = [0, 0, 0, 0, 0] #configurazione di prezzo ottimale
         self.optimal_configuration_margin = 0 #margine ottimale della config sopra
-        self.prices = prices
         self.isOptima = False
 
     def pull_arm(self):
@@ -16,25 +17,28 @@ class GreedyLearner:
             return self.optimal_configuration
 
         try_configuration = self.optimal_configuration.copy()
-        if try_configuration[self.t] < 4:
+        if try_configuration[self.t] < 3:
             try_configuration[self.t] += 1
 
         return try_configuration
 
-    def update(self, margin):
+    def update(self, overallMargin):
         if self.t == -1:
-            self.optimal_configuration_margin = margin
+            self.optimal_configuration_margin = overallMargin
             self.t += 1
             return
 
-        self.configuration_margin[self.t] = margin
+        self.configuration_margin[self.t] = overallMargin
         self.t += 1
         #caso speciale se t = 4 -> aggiornare con la migliore config e abbiamo una nuova configurazione migliore
         if self.t == 5:
-            if np.amax(self.configuration_margin) >= self.optimal_configuration_margin:
+            if np.amax(self.configuration_margin) >= self.optimal_configuration_margin and not self.isOptima:
                 result = np.where(self.configuration_margin == np.amax(self.configuration_margin))
-                self.optimal_configuration[result] += 1
-                self.optimal_configuration_margin = margin
+                result = result[0][0]
+                #result = np.random.choice(result)
+                if self.optimal_configuration[result] < 3:
+                    self.optimal_configuration[result] += 1
+                self.optimal_configuration_margin = np.amax(self.configuration_margin)
             else: self.isOptima = True
             self.t = 0
             self.configuration_margin = [0, 0, 0, 0, 0]
