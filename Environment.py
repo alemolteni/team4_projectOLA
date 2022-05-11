Fixed #units per products and alphas from dirichlet

Environment.py
@@ -26,7 +26,7 @@ class Environment:
                 productList = [Product(int(key), uc["secondary"][key]) for key in uc["secondary"]]
                 self.classes.append(UserClass(conversionRate=uc["conversionRates"], clickProbability=GraphProbabilities(uc["clickProbability"]), alphas=uc["alphas"],
                               Lambda=uc["lambda"], n_user_mean=uc["usersMean"], n_user_variance=uc["usersVariance"], productList=productList,
-                              features_generator=uc["features"], units_gamma_scale=uc["unitsScale"], units_gamma_shape=uc["unitsShape"]))
+                              features_generator=uc["features"], units_gamma_shape=uc["unitsShape"]))
         
         assert len(classes) > 0
         self.n_product = len(classes[0].alphas)
@@ -65,6 +65,7 @@ class Environment:
         episodes = []
         for userClass in self.classes:
             dailyUser = math.ceil(np.random.normal(userClass.n_user[0], userClass.n_user[1]))
+            userClass.generateNewAlphas()
             for i in range(0,dailyUser):
                 episodes.append(userClass.generateEpisode())
         return episodes

Model/Evaluator/Baseline.py
@@ -3,9 +3,9 @@ from Model.Evaluator.Evaluator import Evaluator
 
 
 class Baseline(Evaluator):
-    def __init__(self, products_list=[], click_prob_matrix=None, lambda_prob=0.5, conversion_rates=[], alphas=[], margins=[], verbose=False):
+    def __init__(self, products_list=[], click_prob_matrix=None, lambda_prob=0.5, conversion_rates=[], alphas=[], margins=[], units_mean=[], verbose=False):
             super(Baseline, self).__init__(products_list=products_list, click_prob_matrix=click_prob_matrix, lambda_prob=lambda_prob, conversion_rates=conversion_rates, 
-                alphas=alphas, margins=margins, verbose=verbose)
+                alphas=alphas, margins=margins, units_mean=units_mean, verbose=verbose)
 
     def computeMargin(self):
-        return np.multiply(np.multiply(self.margins,self.conversion_rates),self.alphas).sum()
+        return np.multiply(np.multiply(np.multiply(self.margins,self.units_mean),self.conversion_rates),self.alphas).sum()

Model/Evaluator/Evaluator.py
@@ -2,9 +2,10 @@ import numpy as np
 
 class Evaluator:
     def __init__(self, products_list=[], click_prob_matrix=None, lambda_prob=0.5, conversion_rates=[], 
-                alphas=[], margins=[], verbose=False):
+                alphas=[], margins=[], verbose=False, units_mean=[]):
         assert len(products_list) == len(conversion_rates) and len(products_list) == len(alphas)
         assert len(products_list) == len(margins)
+        assert len(alphas) == len(units_mean)
         assert click_prob_matrix is not None
 
         self.click_prob_matrix = np.array(click_prob_matrix).tolist()
@@ -14,6 +15,7 @@ class Evaluator:
         self.n_products = len(products_list)
         self.alphas = np.array(alphas)
         self.margins = np.array(margins)
+        self.units_mean = np.array(units_mean)
         self.verbose = verbose
 
     def computeMargin(self):

Model/Evaluator/GraphEvaluator.py
@@ -6,9 +6,9 @@ from Model.Evaluator.Evaluator import Evaluator
 
 class GraphEvaluator(Evaluator):
     def __init__(self, products_list=[], click_prob_matrix=None, lambda_prob=0.5, conversion_rates=[], 
-                alphas=[], margins=[], verbose=False):
+                alphas=[], margins=[], units_mean=[], verbose=False):
         super(GraphEvaluator, self).__init__(products_list=products_list, click_prob_matrix=click_prob_matrix, lambda_prob=lambda_prob, conversion_rates=conversion_rates, 
-                alphas=alphas, margins=margins, verbose=verbose)
+                alphas=alphas, margins=margins, units_mean=units_mean, verbose=verbose)
 
         lambda_mat = np.full((self.n_products,self.n_products), 0, dtype=float)
         for i in range(0,len(products_list)):
@@ -69,7 +69,7 @@ class GraphEvaluator(Evaluator):
         for i in range(0,len(self.products_list)):
             visiting_prob = self.computeSingleProduct(i)
             # Margin if alpha = [1 0 0 0 0]
-            single_margins[i] = np.multiply(visiting_prob,np.multiply(self.margins,self.conversion_rates)).sum()
+            single_margins[i] = np.multiply(visiting_prob,np.multiply(np.multiply(self.margins,self.units_mean),self.conversion_rates)).sum()
             if self.verbose: print("Expected value margin for product {} as starting is {} \n".format(i, single_margins[i]))
         # Weight the single margin by alpha
         return np.multiply(single_margins, self.alphas).sum()

Model/UserClass.py
@@ -39,7 +39,7 @@ class UserClass:
         Generate a session/interaction for a new user. It returns a InteractionNode object
     """
 
-    def __init__(self, conversionRate=[], clickProbability=None, alphas=[], units_gamma_shape=3, units_gamma_scale=1,
+    def __init__(self, conversionRate=[], clickProbability=None, alphas=[], units_gamma_shape=[],
                 n_user_mean=15, n_user_variance=4, productList=[], Lambda=0.4, debug=False, features_generator=[]):
         """
         Parameters
@@ -86,6 +86,7 @@ class UserClass:
         self.alphas = alphas
         assert np.array(alphas).sum() > 0.9999999
         assert len(conversionRate) == len(alphas) and len(alphas) == len(productList)
+        assert len(alphas) == len(units_gamma_shape)
         # From alpha_i = [.3, .4, .3] generate [.3, .7, 1]
         self.product_alphas_intervals = np.full((len(self.alphas)), 0, dtype=float)
         cumulative = 0
@@ -103,7 +104,6 @@ class UserClass:
         assert 0 <= self.Lambda <= 1
         self.currentPrice = [0 for i in range(0,len(self.alphas))]
         self.units_gamma_shape = units_gamma_shape
-        self.units_gamma_scale = units_gamma_scale
         self.debug = debug
         
         self.features_prob = []
@@ -129,6 +129,10 @@ class UserClass:
         userInteractions = self.generateProductInteraction(currentProduct, history)
         userInteractions.setFeatures(self.features_names, self.generateFeature())
         return userInteractions
+
+    def generateNewAlphas(self):
+        self.product_alphas_intervals = np.cumsum(np.random.dirichlet(self.alphas))
+        return 
 
     def setCurrentPrice(self, currentPrice):
         self.currentPrice = currentPrice
@@ -171,7 +175,7 @@ class UserClass:
         # If the primary product displayed is bought that the click probability of the two linked secondary products
         # must be addressed
         if bought == 1:
-            units = np.random.gamma(self.units_gamma_shape, self.units_gamma_scale, None)
+            units = np.random.gamma(self.units_gamma_shape[currentProduct], 1, None)
             units = math.ceil(units) # Ceil bc we want at least one unit
             assert units > 0
 