b3fde536a9586cfc6a53856e5dcc2f7c2f1d7547
Samuele Mariani <samuele1.mariani@mail.polimi.it>
2022-05-16 16:44:09 +0200

    Step 3, Thompson sampling, implemented class TS_CR

--- Learner/TS_CR.py
+++ Learner/TS_CR.py
@@ -1,33 +1,50 @@
 import numpy as np
 import math
-import TS 
+from Learner.TS import *
 
 
 class TS_CR(TS):
 
-    def __init__(self, num_products=5, num_prices=5, debug=False):
-        super(TS_CR,self).__init__(num_products=num_products, num_prices=num_prices)
+    def __init__(self, num_products=5, num_prices=4, margins=np.ones((5, 4)), alphas=np.ones(6), debug=False):
+        super(TS_CR, self).__init__(num_products=num_products, num_prices=num_prices)
+        self.margins = margins
+        self.alphas = alphas
+        self.beta_parameters = np.ones((num_products, num_prices, 2))
         # Take as input also alphas or other information known needed for computing expected rewards
-        # in this class only converstion rates are unknows
+        # in this class only conversion rates are unknown
 
         # Look at super class TS to see ho it works, but briefly we use 
         # self.conversion_rates_distro to save parameters alpha and beta of the distribution
         # then they are used to sample a conversion rate for each arm
 
     def pull_arm(self):
-        return super(TS_CR,self).pull_arm()
+        pulled_config = [0, 0, 0, 0, 0]
+        exp_rewards = self.compute_expected_rewards()
+        for i in range(0, self.num_products):
+            pulled_config[i] = np.argmax(exp_rewards[i])
+            
+        return pulled_config
 
     def compute_expected_rewards(self):
         # It should return a matrix #PROD x #LEVELS in which the elements are the computed rewards
         # Then pull arm will use this to choose the arm with the max expected reward as next
-        return 
+        exp_rewards = np.zeros((self.num_products, self.num_prices))
+        for i in range(0, self.num_products):
+            for j in range(0, self.num_prices):
+                exp_rewards[i, j] = np.random.beta(self.beta_parameters[i, j, 0], self.beta_parameters[i, j, 1]) * self.margins[i][j]
+        return exp_rewards
 
-    def update(self, interactions):
-        # From daily interactions extract needed informatio, depending from step uncertainty:
+    def update(self, interactions, pulledArm):
+        # From daily interactions extract needed information, depending on step uncertainty:
         #   - Step 3: update belief over conversion rates
         #   - Step 4: update conversion rates, 𝛼 ratios, #units sold per product
         #   - Step 5: update conversion rates, 𝛼 ratios, #units sold per product, graph weights
 
-        # In this step we don't need to estimate other parameters, so super class can do all the work
-        super(TS_CR,self).update(interactions)
-        return 
+        for i in interactions:
+            if i.bought:
+                reward = 1.0
+            else:
+                reward = 0.0
+            self.beta_parameters[i.product, pulledArm[i.product], 0] = self.beta_parameters[i.product, pulledArm[i.product], 0] + reward
+            self.beta_parameters[i.product, pulledArm[i.product], 1] = self.beta_parameters[i.product, pulledArm[i.product], 1] + 1.0 - reward
+        return
--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ from Model.constants import *
 from Model.GraphProbabilities import *
 
 from Learner.GreedyLearner import *
+from Learner.TS_CR import *
 from Learner.BruteForce import *
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,14 +15,47 @@ def totalMarginPerNode(envReturn, marginsPerPrice, pulledArm):
     TotMargin = 0
 
     for e in envReturn:
-        #print("Product: ", e.product,
-         #     "Price: ", pulledArm[e.product],
-          #    "Units: ", e.units,
-           #   "Margin: ", marginsPerPrice[e.product][pulledArm[e.product]] * e.units)
+        # print("Product: ", e.product,
+        #     "Price: ", pulledArm[e.product],
+        #    "Units: ", e.units,
+        #   "Margin: ", marginsPerPrice[e.product][pulledArm[e.product]] * e.units)
         if e.bought:
             TotMargin += marginsPerPrice[e.product][pulledArm[e.product]] * e.units
     return TotMargin
 
+
+def testTS_CR():
+    margins = [[1, 2, 10, 16],
+               [10, 20, 35, 40],
+               [12, 15, 18, 21],
+               [3, 8, 15, 21],
+               [8, 10, 17, 26]]
+    conv_rates = [[0.7, 0.7, 0.4, 0.2],
+                  [0.9, 0.8, 0.6, 0.2],
+                  [0.9, 0.7, 0.5, 0.3],
+                  [0.8, 0.7, 0.4, 0.3],
+                  [0.9, 0.65, 0.45, 0.2]]
+
+    for i in range(0, 5):
+        print('{} - {} - {} - {}'.format(margins[i][0] * conv_rates[i][0], margins[i][1] * conv_rates[i][1],
+                                         margins[i][2] * conv_rates[i][2], margins[i][3] * conv_rates[i][3]))
+
+    print('\n')
+    tsLearner = TS_CR(margins=margins)
+    n_experiments = 100
+    environment = Environment(config_path="Configs/config1.json")
+
+    for i in range(0, n_experiments):
+        pulledArm = tsLearner.pull_arm()
+        environment.setPriceLevels(pulledArm)
+        interactions = environment.round()
+        tsLearner.update(interactions, pulledArm)
+
+    print("Pulled arm {} at time {}:".format(pulledArm, i))
+    print("Optimal configuration in theory: [2, 2, 0, 3, 2]")
+    return;
+
+testTS_CR()
 
 conversionRate = [
             [1, 0.8, 0.6, 0.4],
@@ -52,7 +86,7 @@ Lambda = 0.8
 
 #Ignore all the above
 
-environment = Environment(config_path="Configs/Config2.json")
+environment = Environment(config_path="Configs/config1.json")
 gLearner = BruteForce(debug=True)
 
 marginsPerPrice = [
@@ -63,14 +97,14 @@ marginsPerPrice = [
     [8, 10, 17, 26]
   ]
 
-n_experiments = 3000
+n_experiments = 100
 
 optimal_arm = []
 
 allMargin = np.array([])
 
 for i in range(0, 1030):
-    pulledArm = [0, 0, 0, 0, 0]#= gLearner.pull_arm()
+    pulledArm = gLearner.pull_arm()
     print(pulledArm)
     environment.setPriceLevels(pulledArm)
     envReturn = environment.round()
