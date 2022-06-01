b63c545435411ab6286277b2c8992fc099eab87a
Davide Cairoli <davide.cairoli.sbabu@gmail.com>
2022-06-01 15:27:55 +0200

    #Units computation comments

--- Model/Evaluator/Evaluator.py
+++ Model/Evaluator/Evaluator.py
@@ -16,12 +16,22 @@ class Evaluator:
         self.n_products = len(products_list)
         self.alphas = np.array(alphas)
         self.margins = np.array(margins)
-        actual_means = []
+
         # Cannot integrate the gamma function, so empirically compute the ceiled mean
+        # In environment: num_units = math.ceil(gamma(expected_val))
+        # So the average number of units sold is different from expected_val
+        # To get actual mean we should integrate gamma between [0,1] to get P(units=1) and so on for all intervals
+        # Then the actual units mean will be sum{i} of P(units=i)*i
+        # I've done it empirically by generating a lot of samples (not so clean solution but effective)
+        #       E.G. np.ceil(np.random.gamma(.95, 1, size=1000000)).mean() ---> mean = 1.541436
+        #       If I repeat the process 10 times then std = 0.0011484
+        #       It is much different from ceil(expected_val = 0.95) = 1 <> actual_mean = 1.54
+        actual_means = []
         for i in range(0,len(units_mean)):
             empiric_mean = np.ceil(np.random.gamma(units_mean[i], 1, size=1000000)).mean()
             actual_means.append(int(empiric_mean*100) / 100)
         self.units_mean = np.array(actual_means)
+        
         self.verbose = verbose
 
     def computeMargin(self):
